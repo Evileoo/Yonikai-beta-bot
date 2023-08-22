95f5a02a3b9339ff792a2db9ae5c56bcc3813a1f
Leo <debonnetleo4@gmail.com>
2023-08-22 07:58:29 +0200

    activer -> statistiques du server

---
+++ commands/activer.js
@@ -0,0 +1,101 @@
+const { SlashCommandBuilder,  PermissionsBitField, EmbedBuilder, ChannelType } = require("discord.js");
+const constants = require("../constants");
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName("activer")
+		.setDescription("Permet d'activer des fonctionnalités du bot")
+        .setDefaultMemberPermissions(PermissionsBitField.Flags.Administrator)
+        .addStringOption( (option) =>
+            option
+            .setName("nom")
+            .setDescription("Nom de la fonctionnalité")
+            .setRequired(true)
+            .addChoices(
+                { name: "statistiques du serveur", value: "stats" },
+            )
+        ),
+	async execute(interaction) {
+
+        //récupération du contenu de la commande
+        const activate = interaction.options.getString("nom");
+        
+        //Vérification des droits du bot
+        if(!interaction.guild.members.me.permissions.has(PermissionsBitField.Flags.Administrator)){
+            return interaction.reply({
+                embeds: [{
+                    title: `Erreur !`,
+                    description: `Droits d'administrateur requis pour l'exécution de la commande **activate**`,
+                    color: constants.MAINCOLOR
+                }],
+                ephemeral: true
+            });
+        }
+
+        //Activation des fonctionnalités
+        switch(activate){
+            case "stats":
+                //Vérification de l'existence du salon avant la création de celui-ci
+                let channel = interaction.guild.channels.cache.find(channel => channel.name === "statistiques");
+                if(channel){
+                    alreadyActivated("Le salon");
+                } else {
+                    //Création du salon
+                    const channelParent = interaction.channel.parent.id;
+                    let everyoneRole = interaction.guild.roles.cache.find(r => r.name === '@everyone');
+                    await interaction.guild.channels.create({
+                        name: `statistiques`,
+                        parent: `${channelParent}`,
+                        type: ChannelType.GuildText,
+                        permissionOverwrites : [{
+                            id: everyoneRole.id,
+                            deny: PermissionsBitField.Flags.SendMessages
+                        }]
+                    });
+
+                    //Vérification de la bonne exécution de la commande
+                    channel = interaction.guild.channels.cache.find(channel => channel.name === "statistiques");
+                    if(channel){
+                        //confirmer la création du salon
+                        interaction.reply({
+                            content : "Salon créé, le salon est identifié par son nom, veuillez ne pas toucher au nom de celui-ci",
+                            ephemeral: true
+                        });
+
+                        //Comptage et affichage du nombre de membres présents sur le serveur
+                        const memberCount = interaction.guild.members.cache;
+
+                        return interaction.guild.channels.cache.get(channel.id).send({
+                            content: `Nombre de membres : ${memberCount.size}`
+                        });
+                    } else {
+                        return interaction.reply({
+                            content : "Une erreur est survenue, veuillez faire un rapport",
+                            ephemeral: true
+                        });
+                    }
+
+
+                }
+            break;
+
+            default:
+                //Afficher un message lorsque la commande demandée n'est pas reconnue
+                return interaction.reply({
+                    content : "Une erreur est survenue, veuillez faire un rapport\nRaison : erreur switch pour la commande **activate**",
+                    ephemeral: true
+                });
+        }
+        //Centralisation de tous les messages d'autorisations déjà accordées
+        function alreadyActivated(parameter){
+            return interaction.reply({
+                embeds: [{
+                    title: `Cette autorisation a déjà été accordée`,
+                    description: `${parameter} a été trouvé sur le serveur`,
+                    color: constants.MAINCOLOR
+                }],
+                ephemeral: true
+            });
+        }
+	},
+};
